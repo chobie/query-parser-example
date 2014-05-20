c7df58429998a0b9346362a7f3a7503f3636af18
Shuhei Tanuma <shuhei.tanuma@gmail.com>
2014-05-20 13:27:11 -0400

    improve test (448e6716c06c13cf552f8b4fac65cec264b51ae1)

--- src/query/main.c
+++ src/query/main.c
@@ -3,9 +3,15 @@
 #include "y.tab.h"
 
 int main() {
-	query q = parse_query("select from t where foo = '5' ;");
+	//query q = parse_query("select a from t where foo = '5';");
+	query q = parse_query("select value, time from t where c = '5';");
 	printf("table name: %s\n", q.f->table);
 	printf("where column: %s, value: %s\n", q.w->column_name, q.w->v->svalue);
+	printf("arr: %d\n", q.c->size);
+	int i = 0;
+	for (i = 0; i < q.c->size; i++) {
+		printf("%s\n", q.c->elems[i]);
+	}
 	close_query(&q);
 	return 0;
 }
--- src/query/parser_test.go
+++ src/query/parser_test.go
@@ -1,6 +1,7 @@
 package query
 
 import (
+	"fmt"
 	. "launchpad.net/gocheck"
 	"testing"
 )
@@ -14,10 +15,48 @@ type QueryParserSuite struct{}
 var _ = Suite(&QueryParserSuite{})
 
 func (self *QueryParserSuite) TestParseBasicSelectQuery(c *C) {
-	q, err := ParseQuery("select from t where c = '5';")
+	q, err := ParseQuery("select value, time from t where c = '5';")
 	c.Assert(err, IsNil)
+	fmt.Printf("%+v\n", q.GetColumnNames())
+	//c.Assert(q.GetColumnNames(), DeepEquals, []string{"value", "time"})
 	w := q.GetWhereClause()
 	c.Assert(q.GetFromClause().TableName, Equals, "t")
 	c.Assert(w.ColumnName, Equals, "c")
 	c.Assert(w.Value, Equals, "5")
 }
+
+func (self *QueryParserSuite) TestParseSelectWithUpperCase(c *C) {
+	q, err := ParseQuery("SELECT VALUE, TIME FROM t WHERE C = '5';")
+	c.Assert(err, IsNil)
+	c.Assert(q.GetColumnNames(), DeepEquals, []string{"VALUE", "TIME"})
+	w := q.GetWhereClause()
+	c.Assert(w.ColumnName, Equals, "C")
+	c.Assert(w.Value, Equals, "5")
+}
+
+
+func (self *QueryParserSuite) TestParseSelectWithInequality(c *C) {
+	q, err := ParseQuery("SELECT VALUE, TIME FROM t WHERE C < '5';")
+	//q, err := ParseQuery("select value, time from t where c < 5;")
+	c.Assert(err, IsNil)
+	//c.Assert(q.GetColumnNames(), DeepEquals, []string{"value", "time"})
+	w := q.GetWhereClause()
+	c.Assert(q.GetFromClause().TableName, Equals, "t")
+	c.Assert(w.ColumnName, Equals, "C")
+	c.Assert(int(w.Op), Equals, LESS_THAN)
+	// TODO: fix this
+	c.Assert(w.Value, Equals, "5")
+}
+
+func (self *QueryParserSuite) TestParseSelectWithTimeCondition(c *C) {
+	q, err := ParseQuery("select value, time FROM t WHERE time > 1;")
+	//q, err := ParseQuery("select value, time from t where time > now() - 1d;")
+	c.Assert(err, IsNil)
+	c.Assert(q.GetColumnNames(), DeepEquals, []string{"value", "time"})
+	w := q.GetWhereClause()
+	c.Assert(q.GetFromClause().TableName, Equals, "t")
+	c.Assert(w.ColumnName, Equals, "time")
+	//c.Assert(w.Value, Equals, "5")
+}
+
+
